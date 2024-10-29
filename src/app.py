a0ffca9849b98136670db879498b991069066e9b
Pablo Esteban Murillo Mata <pabloe.mm@live.com>
2024-10-28 20:58:43 -0600

    Segundo commit api

--- src/app.py
+++ src/app.py
@@ -1,37 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+from config import config
  
 app =Flask(__name__)
+con = MySQL(app)
  
-@app.route("/")
-def index():
-    titulo="SOy un titulo"
-    list=['Pedro','Juan','Fulanito']
-    return render_template('uno.html',titulo=titulo,list=list)
- 
-@app.route("/user/<string:user>")
-def user(user):
-    return "el usuario es {}".format(user)
- 
-@app.route("/numero/<int:n1>")
-def numero(n1):
-    return "el numero es {}".format(n1)
- 
- 
-@app.route("/user/<string:nom>/<int:id>")
-def user2(nom, id):
-    return "<h1> ID:{} Nombre: {}</h1>".format(id, nom)
- 
-@app.route("/suma/<int:n1>/<int:n2>")
-def suma(n1, n2):
-    return "<h1> la suma es: {}</h1>".format(n1 + n2)
+@app.route("/alumnos",methods=['GET'])
+def lista_alumnos():
+    try:
+        cursor = con.connection.cursor()
+        sql = "select * from alumnos"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        alumnos = []
+        for fila in datos:
+            alumno={"matricula":fila[0],"nombre":fila[1],"apaterno":fila[2],"amaterno":fila[3],"correo":fila[4]}
+            alumnos.append(alumno)
+        return jsonify({'alumnos':alumnos,'mensake':'Lista de Alumnos','exito':True})
+    except Exception as ex:
+        return jsonify({"massage":"errors {}".format(ex),'exito':False}),500
 
-@app.route("/default")
-@app.route("/default/<string:nom>")
-def nom2(nom='kas'):
-    return "<h1> el nombre es {}</h1>".format(nom)
- 
- 
- 
+@app.route("/alumnos/<mat>",methods=['GET'])
+def leer_alumno(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        cursor = con.connection.cursor()
+        sql = "select * from alumnos"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        alumnos = []
+        for fila in datos:
+            alumno={"matricula":fila[0],"nombre":fila[1],"apaterno":fila[2],"amaterno":fila[3],"correo":fila[4]}
+            alumnos.append(alumno)
+        return jsonify({'alumnos':alumnos,'mensake':'Lista de Alumnos','exito':True})
+    except Exception as ex:
+        return jsonify({"massage":"errors {}".format(ex),'exito':False}),500
+  
 if __name__=="__main__":
     app.run(debug=True)
+    app.register_error_handler(404,pagina_no_encontrada)
+    app.run(host="0.0.0.0",port=5000)
    
